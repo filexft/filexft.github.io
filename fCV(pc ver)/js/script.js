474c21970d8dc102fc80395f934404375275ef70
filexft <82711440+filexft@users.noreply.github.com>
2022-04-07 14:12:13 +0200

    Add files via upload

---
+++ fCV(pc ver)/js/script.js
@@ -0,0 +1,31 @@
+/*  ========= typing animation  ========= */
+var typed = new Typed(".typing",{
+    strings: ["Web Designer", "Web Developer", "Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop: true
+})
+
+/* https://www.youtube.com/watch?v=DmiVBbNLzo8&t=0s   15:31 */
+/*  ========= aside  ========= */
+/*
+const nav = document.querySelector(".nav"),
+        navList =  nav.querySelectorAll("li"),
+        totalNavList = navList.length;
+        {
+            const a = navList[i].querySelector("a");
+            a.addEventListener("click", function ()
+            {
+                for(let j = 0; j < totalNavList; j++)
+                {
+                    navList[j].querySelector("a").classList.remove("active");
+                }
+                this.classList.add("active")
+                showSection(this);
+            })
+        }
+        function showSection(element)
+        {
+            element.getAttribute("href").split("#");
+        }
+*/
